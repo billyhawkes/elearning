9922ce664cbbc97316443be682939238a01dc92b
Billy Hawkes <billyhawkes02@gmail.com>
2024-03-03 14:56:47 -0500

    Setup hosting

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-7dc%9!bhw@r=*8$1+dp_=ybxpwitqt1u1$j)wa()3zqm&5kmqc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,6 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 STATIC_URL = "static/"
 
