e6262de4f430b81655ecd45e64bfffcf38a6c214
Billy Hawkes <billyhawkes02@gmail.com>
2024-03-03 15:12:05 -0500

    Attempted fixes

--- elearning/settings.py
+++ elearning/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",
     "crispy_forms",
     "crispy_tailwind",
     "app",
@@ -128,8 +129,6 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
